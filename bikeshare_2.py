a9b2009a094fac80755aced30f291145437b1998
olcay.nisanoglu <olcay.nisanoglu@turkcell.com.tr>
2020-06-01 01:16:33 +0300

    added input codes and if checks for choosing city

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -146,3 +146,20 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+    
+        print_raw_data = input('\nWould you like to see raw data? Enter yes or no.\n')
+        if print_raw_data.lower() == 'yes':
+           with open("{}.csv".format(city), "r") as f:
+                i = 0
+                while print_raw_data.lower() != 'no':
+                      for i, line in enumerate(f):
+                          i += 1
+                          print(line)
+                          if i % 5 != 0:
+                             continue
+                          else:
+                             break
+                      print_raw_data = input('\nWould you like to continue to see raw data? Enter yes or no.\n')
+        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        if restart.lower() != 'yes':
+            break
