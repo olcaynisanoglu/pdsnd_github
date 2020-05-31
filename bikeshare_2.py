2d72d3cbe260e60f9ceab571ec1b0b59a5a274ae
olcay.nisanoglu <olcay.nisanoglu@turkcell.com.tr>
2020-05-31 21:39:44 +0300

    copied project file bikeshare_2.py

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,122 +1,9 @@
-import time
-import pandas as pd
-import numpy as np
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 18:36:57 2020
 
-CITY_DATA = { 'chicago': 'chicago.csv',
-              'new york city': 'new_york_city.csv',
-              'washington': 'washington.csv' }
-
-def get_filters():
-    """
-    Asks user to specify a city, month, and day to analyze.
-
-    Returns:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    """
-    print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
-    # get user input for month (all, january, february, ... , june)
-
-
-    # get user input for day of week (all, monday, tuesday, ... sunday)
-
-
-    print('-'*40)
-    return city, month, day
-
-
-def load_data(city, month, day):
-    """
-    Loads data for the specified city and filters by month and day if applicable.
-
-    Args:
-        (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "all" to apply no month filter
-        (str) day - name of the day of week to filter by, or "all" to apply no day filter
-    Returns:
-        df - Pandas DataFrame containing city data filtered by month and day
-    """
-
-
-    return df
-
-
-def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
-
-    print('\nCalculating The Most Frequent Times of Travel...\n')
-    start_time = time.time()
-
-    # display the most common month
-
-
-    # display the most common day of week
-
-
-    # display the most common start hour
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
-
-    print('\nCalculating The Most Popular Stations and Trip...\n')
-    start_time = time.time()
-
-    # display most commonly used start station
-
-
-    # display most commonly used end station
-
-
-    # display most frequent combination of start station and end station trip
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
-
-    print('\nCalculating Trip Duration...\n')
-    start_time = time.time()
-
-    # display total travel time
-
-
-    # display mean travel time
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
-
-def user_stats(df):
-    """Displays statistics on bikeshare users."""
-
-    print('\nCalculating User Stats...\n')
-    start_time = time.time()
-
-    # Display counts of user types
-
-
-    # Display counts of gender
-
-
-    # Display earliest, most recent, and most common year of birth
-
-
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
-
+@author: TCONISANOGLU
+"""
 
 def main():
     while True:
@@ -128,10 +15,19 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
 
+        print_raw_data = input('\nWould you like to see raw data? Enter yes or no.\n')
+        if print_raw_data.lower() == 'yes':
+           with open("{}.csv".format(city), "r") as f:
+                i = 0
+                while print_raw_data.lower() != 'no':
+                      for i, line in enumerate(f):
+                          i += 1
+                          print(line)
+                          if i % 5 != 0:
+                             continue
+                          else:
+                             break
+                      print_raw_data = input('\nWould you like to continue to see raw data? Enter yes or no.\n')
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
-
-if __name__ == "__main__":
-	main()
